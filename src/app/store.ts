c0ef4ccbe1a188ed3a99f0bc26906778328b585b
jun-g-0 <junworks7@gmail.com>
2021-08-30 11:02:40 +0900

    feat: added redux persisit funcs

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,22 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import settingsReducer from '../features/settings/settingsSlice';
 
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+const persistedSettingsReducer = persistReducer(persistConfig, settingsReducer)
+
 export const store = configureStore({
   reducer: {
-    settings: settingsReducer,
+    settings: persistedSettingsReducer,
   },
 });
+
+export const persistor = persistStore(store);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
